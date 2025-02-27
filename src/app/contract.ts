c6744fe9b74cbe3729d1ff7284e1b91eaa26eb9f
VaultFI.pro <onecryp7o@gmail.com>
2025-02-27 13:02:26 +0100

    Contract

--- src/app/contract.ts
+++ src/app/contract.ts
@@ -2,11 +2,15 @@ import { getContract } from "thirdweb";
 import { defineChain } from "thirdweb/chains";
 import { client } from "./client"
 
+
+const cAddress = process.env.NEXT_PUBLIC_Contract_Address;
+
+if (!cAddress) {
+  throw new Error("No address provided");
+}
 // connect to your contract
-const contract = getContract({
+export const contract = getContract({
     client,
     chain: defineChain(8453),
-    address: 'process.env.NEXT_PUBLIC_Contract_Adress',
+    address: cAddress,
   });
-
-  export default contract
