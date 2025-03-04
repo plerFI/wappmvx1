50ab07a24f50b2c6e66a0ebe19c12e6b6032521a
VaultFI.pro <onecryp7o@gmail.com>
2025-03-04 22:53:47 +0100

    next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,24 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // fixes wallet connect dependency issue https://docs.walletconnect.com/web3modal/nextjs/about#extra-configuration
+  // Fixes wallet connect dependency issue
+  // https://docs.walletconnect.com/web3modal/nextjs/about#extra-configuration
   webpack: (config) => {
     config.externals.push("pino-pretty", "lokijs", "encoding");
     return config;
+  },
+
+  async headers() {
+    return [
+      {
+        source: "/(.*)", // Gilt für alle Routen
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value: "script-src 'self' 'unsafe-eval';", 
+          },
+        ],
+      },
+    ];
   },
 };
 
